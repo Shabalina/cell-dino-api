ef62ebf9300b775fd2ffae955f506f8969871406
Anastasia Shapovalova <shabalinastya@Romans-MacBook-Air.local>
2026-03-09 14:38:59 +0000

    feat: add tf file for the vars

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "image_tag" {
+  type        = string
+  description = "The unique Docker image tag (for this project the git commit SHA)"
+}
+
+variable "ecr_url" {
+  type        = string
+  description = "The full URL of the ECR repository"
+}
+
+variable "sagemaker_role_arn" {
+  type        = string
+  description = "The ARN of the IAM role SageMaker uses to run the container"
+}
